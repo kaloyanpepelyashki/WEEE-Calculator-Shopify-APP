b5d474447647a59656d9148b15cc7a4f097ea69a
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-03-15 12:08:59 +0100

    Set up the WEEE-Calculator-Shopify-app

---
+++ app/Services/DPACalculatorAccessor.ts
@@ -0,0 +1,22 @@
+export default class DpaCalulatorAccessor {
+  protected url: string;
+  constructor() {
+    this.url = "http://localhost:3000/";
+  }
+
+  public async fetchData(): Promise<Array<number>> {
+    try {
+      let dataList: Array<number> = [];
+      await fetch(this.url, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data: Array<number>) => {
+          dataList = data;
+        });
+      return dataList;
+    } catch (e) {
+      throw new Error(`Error fetching data: ${e.message}`);
+    }
+  }
+}
---
+++ app/routes/app._index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+import DpaCalculatorAccessor from "../Services/DPACalculatorAccessor";
+
+export default function Index() {
+  const [data, setData] = useState([0]);
+  const dpaCalcualtor = new DpaCalculatorAccessor();
+  const outputData = async () => {
+    setData(await dpaCalcualtor.fetchData());
+  };
+
+  return (
+    <Page>
+      <BlockStack gap="500">
+        <Layout>
+          <Layout.Section>
+            <button onClick={outputData}>Fetch</button>
+            <h2>
+              {data.map((entry: number) => {
+                return <h2 key={entry}>{entry}</h2>;
+              })}
+            </h2>
+          </Layout.Section>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
