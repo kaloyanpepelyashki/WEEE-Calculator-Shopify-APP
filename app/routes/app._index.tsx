b06bb406795c9669308f349b9af1aa15c4cf51cc
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-05-08 10:12:48 +0200

    Some commenting on the code

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -9,28 +9,36 @@ import { useAppBridge } from "@shopify/app-bridge-react";
 import { EmbeddedAdminContext } from "node_modules/@shopify/shopify-app-remix/build/ts/server/authenticate/admin/types";
 import ProductsService from "../Services/ProductsService";
 
+//Every page should have exaclty one loader function, the loader function is where data is being fetched
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  //First you authenticated the session
   const { session } = await authenticate.admin(request);
 
+  //Fromm session you can extract:
+  // "shop" - shopify host name
+  // "accessToken" - the shopify api key
   const { shop, accessToken } = session;
 
-  const productsService = new ProductsService(
+  //This is the product service
+  const productsService: ProductsService = new ProductsService(
     accessToken as string,
     shop as string,
   );
+
+  //This will get a list of shop products
   const productList = await productsService.getAllShopProducts();
 
   console.log(productList);
   if (productList.length > 0) {
     return json({ accessToken, shop, productList });
-  } else {
-    return json({ accessToken, shop });
   }
 };
 
 export default function Index() {
+  //You extract data from the loader using useLoaderData
   const loaderData = useLoaderData<typeof loader>();
-  console.log("accessToken, shop, productList :", loaderData);
+
+  console.log("accessToken, shop, productList :", loaderData.productList);
 
   return (
     <Page>
